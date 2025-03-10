416de0d6c4e5661827d838d7e180c29e5414587f
hugolpz <hugo.lpz@gmail.com>
2025-03-10 20:21:50 +0100

    continue

--- content.js
+++ content.js
@@ -1,7 +1,53 @@
 chrome.storage.sync.get('fontFamily', ({ fontFamily }) => {
   if (fontFamily) {
-    const style = document.createElement('style');
+    let style = document.getElementById('add-zhuyin');
+    if (!style) {
+      style = document.createElement('style');
+      style.id = 'add-zhuyin';
+      document.head.append(style);
+    }
     style.textContent = `:lang(zh-Hant) * { font-family: '${fontFamily}' !important; }`;
-    document.head.append(style);
   }
+
+  // Inject the CSS file
+  let link = document.getElementById('zhuyin-styles');
+  if (!link) {
+    link = document.createElement('link');
+    link.id = 'zhuyin-styles';
+    link.rel = 'stylesheet';
+    link.href = chrome.runtime.getURL('css/styles.css');
+    document.head.append(link);
+  }
+
+  // Inject the fonts dynamically
+  const fonts = [
+    {
+      family: 'kaishu',
+      urls: [
+        'https://www.chinois-sans-frontieres.tw/fonts/kaishu/AR_PL_ZenKai_Uni.woff2',
+        'https://www.chinois-sans-frontieres.tw/fonts/kaishu/AR_PL_ZenKai_Uni.woff',
+        'https://www.chinois-sans-frontieres.tw/fonts/kaishu/AR_PL_ZenKai_Uni.ttf'
+      ]
+    },
+    {
+      family: 'zhuyin',
+      urls: [
+        'https://www.chinois-sans-frontieres.tw/fonts/zhuyin/HanWangKaiMediumChuIn.woff2',
+        'https://www.chinois-sans-frontieres.tw/fonts/zhuyin/HanWangKaiMediumChuIn.woff',
+        'https://www.chinois-sans-frontieres.tw/fonts/zhuyin/HanWangKaiMediumChuIn.ttf'
+      ]
+    }
+  ];
+
+  fonts.forEach(font => {
+    font.urls.forEach(url => {
+      let fontLink = document.createElement('link');
+      fontLink.rel = 'preload';
+      fontLink.href = url;
+      fontLink.as = 'font';
+      fontLink.type = 'font/woff2';
+      fontLink.crossOrigin = 'anonymous';
+      document.head.append(fontLink);
+    });
+  });
 });
---
+++ web-ext-config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+	build: {
+		overwriteDest: true
+	},
+	ignoreFiles: [
+		'package.json',
+		'package-lock.json',
+		'package-lock.json',
+		'dist/',
+		'README.md',
+		'web-ext-config.js',
+	],
+	artifactsDir: 'dist'
+};
